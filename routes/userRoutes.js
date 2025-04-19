84f55b6c94522aee39eaf314e1c5178912b4dc90
Joel Ekeng <joelekeng23@gmail.com>
2025-04-19 13:39:29 +0000

    Update env variable

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,8 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
 
 const UserSchema = new mongoose.Schema({
     fullName: {
