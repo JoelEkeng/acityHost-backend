82f493457d737dfca9e82b8517ff6c7bffef91d8
Joel Ekeng <joelekeng23@gmail.com>
2025-04-28 23:02:32 +0000

    Fixed patching issues

--- controllers/ticketController.js
+++ controllers/ticketController.js
@@ -49,6 +49,39 @@ exports.updateTicket = async (req, res) => {
   }
 };
 
+// PATCH method is used to update a resource partially
+exports.patchTicket = async (req, res) => {
+  try {
+    const ticket = await MaintenanceTicket.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    if (!ticket) {
+      return res.status(404).json({ message: 'Ticket not found' });
+    }
+    if (req.body.status) {
+      await User.findByIdAndUpdate(
+        ticket.createdBy,
+        { $push: { maintenanceLogs: ticket._id } },
+        { new: true }
+      );
+    }
+    if (req.body.priority) {
+      await User.findByIdAndUpdate(
+        ticket.createdBy,
+        { $push: { priorityLogs: ticket._id } },
+        { new: true }
+      );
+    }
+    if (req.body.assignedTo) {
+        req.body.assignedTo,
+        { $push: { assignedTickets: ticket._id } },
+        { new: true }
+    }
+     res.status(200).json(ticket);
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error });
+    console.error(error);
+  }
+};
+
 exports.deleteTicket = async (req, res) => {
   try {
     const ticket = await MaintenanceTicket.findByIdAndDelete(req.params.id);
--- routes/ticketRoutes.js
+++ routes/ticketRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { getTickets, createTicket, getTicketById, updateTicket, deleteTicket } = require('../controllers/ticketController');
+const { getTickets, createTicket, getTicketById, updateTicket, patchTicket, deleteTicket } = require('../controllers/ticketController');
 const { authenticate } = require('../middleware/authMiddleware');
 
 router.get('/tickets', getTickets);
 router.post('/tickets', authenticate, createTicket);
 router.get('/tickets/:id', getTicketById);
 router.put('/tickets/:id', updateTicket);
+router.patch('/tickets/:id', patchTicket);
 router.delete('/tickets/:id', deleteTicket);
 
 module.exports = router;
