2b1650e0e19bf5918f2bd93831cc01dfbd36c159
Joel Ekeng <joelekeng23@gmail.com>
2025-04-30 16:51:44 +0000

    Added authentication

--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { createRoom, getRooms, createBulkRooms } = require('../controllers/roomController');
+const { authenticate, authorizeRoles } = require('../middleware/authMiddleware');
 
 router.post('/room', createRoom);
 router.get('/', getRooms);
-router.post('/rooms/bulk', createBulkRooms);
+router.post('/rooms/bulk', authenticate, authorizeRoles('admin'), createBulkRooms);
 
 module.exports = router;
