46400f3394928bd0844a5333a84fc06f08bec4c4
Joel Ekeng <joelekeng23@gmail.com>
2025-05-03 09:04:52 +0000

    Fixed booking issues

--- controllers/bookingsController.js
+++ controllers/bookingsController.js
@@ -1,15 +1,46 @@
-const bookings = require('../models/Booking');
+const User = require('../models/User');
+const Booking = require('../models/Booking');
 
 exports.createBooking = async (req, res) => {
-    try {
-        const booking = new bookings(req.body);
-        const savedBooking = await booking.save();
-        res.status(201).json(savedBooking);
-    } catch (err) {
-        console.error('Error creating booking:', err);
-        res.status(500).json({ message: 'Server error creating booking' });
+  try {
+    const {
+      rollNumber, // this is the string like "10211100294"
+      roomId,
+      bookingDate,
+      startTime,
+      endTime,
+      bedPosition,
+      payment
+    } = req.body;
+
+    if (!roomId) {
+      return res.status(400).json({ message: 'roomId is required' });
     }
+
+    const user = await User.findOne({ rollNumber });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found with that roll number' });
     }
+
+    const booking = new Booking({
+      rollNumber: user._id,
+      roomId,
+      bookingDate,
+      startTime,
+      endTime,
+      bedPosition,
+      payment
+    });
+
+    await booking.save();
+
+    res.status(201).json(booking);
+  } catch (err) {
+    console.error('Error creating booking:', err);
+    res.status(500).json({ message: 'Server error creating booking' });
+  }
+};
+
 exports.getAllBookings = async (req, res) => {
     try {
         const bookingsList = await bookings.find();
