b8f7ea20e64aad89ffd2ecfdcad0553a524cffbb
Joel Ekeng <joelekeng23@gmail.com>
2025-05-03 12:38:43 +0000

    Update controllers

--- routes/roomRoutes.js
+++ routes/roomRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { createRoom, getRooms, createBulkRooms, updateRoomDetails } = require('../controllers/roomController');
+const { createRoom, getRooms, createBulkRooms, updateRoomDetails,  getRoomAvailability } = require('../controllers/roomController');
 const { authenticate, authorizeRoles } = require('../middleware/authMiddleware');
 
 router.post('/rooms', createRoom);
 router.get('/rooms', getRooms);
 router.post('/rooms/bulk', authenticate, authorizeRoles('admin'), createBulkRooms);
 router.patch('/:roomId', authenticate, authorizeRoles('admin'), updateRoomDetails);
+router.get('/rooms/availability', authenticate, getRoomAvailability);
 
 module.exports = router;
